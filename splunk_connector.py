b0b4b0f64879f82f953fe5a3cdb9cfc18038fb76
Ishan Ghelani <ighelani@splunk.com>
2023-03-30 20:50:15 +0530

    changed the logic for sdi generation

--- splunk_connector.py
+++ splunk_connector.py
@@ -952,7 +952,8 @@ class SplunkConnector(phantom.BaseConnector):
             else:
                 for k, v in list(item.items()):
                     cef[consts.CIM_CEF_MAP.get(k, k)] = v
-
+            if "_serial" in item:
+                item.pop("_serial")
             input_str = json.dumps(item)
             input_str = UnicodeDammit(input_str).unicode_markup.encode('utf-8')
 
@@ -983,7 +984,8 @@ class SplunkConnector(phantom.BaseConnector):
                     if value is not None:
                         cleaned_cef[key] = value
                 cef = cleaned_cef
-
+            if "_serial" in cef:
+                cef.pop("_serial")
             artifact = [{
                     'cef': cef,
                     'name': 'Field Values',
